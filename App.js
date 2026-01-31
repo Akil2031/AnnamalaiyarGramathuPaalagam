d27045bc9dace109f3bd91608d9bbfc02c542f68
Akilkumar Padmanaban <annamalaiayargramathupalagam@gmail.com>
2026-01-31 11:57:17 +0530

    New Splash screen

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
+import { useEffect } from "react";
 import { Platform } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
+
 import TabNavigator from "./src/navigation/TabNavigator";
 
+// Keep splash screen visible
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  useEffect(() => {
+    async function prepare() {
+      // ⏱ SPLASH INTERVAL (change time here)
+      await new Promise(resolve => setTimeout(resolve, 2500)); // 2.5 sec
+
+      // Hide splash screen
+      await SplashScreen.hideAsync();
+    }
+
+    prepare();
+  }, []);
+
   return (
     <NavigationContainer>
       {Platform.OS === "web" ? (
